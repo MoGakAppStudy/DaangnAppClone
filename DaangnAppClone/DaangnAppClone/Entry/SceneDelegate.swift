2035d3756247c0d3aad6cf38b7b156515280225a
Ellen J <jcrescent61@gmail.com>
2024-08-21 23:01:38 +0900

    First project setting

---
+++ DaangnAppClone/DaangnAppClone/Entry/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  DaangnAppClone
+//
+//  Created by Ellen J on 8/21/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ DaangnAppClone/DaangnAppClone/Scene/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DaangnAppClone
+//
+//  Created by Ellen J on 8/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

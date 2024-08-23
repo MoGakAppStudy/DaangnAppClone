4273e23f33ef67e61af810704b1073e48ff5ebc0
Ellen J <jcrescent61@gmail.com>
2024-08-23 18:18:01 +0900

    feat: TabBarViewController 구현

--- DaangnAppClone/DaangnAppClone/Scene/TabBarViewController.swift
+++ DaangnAppClone/DaangnAppClone/Scene/TabBarViewController.swift
@@ -7,10 +7,27 @@
 
 import UIKit
 
-final class TabBarViewController: UIViewController {
+final class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBarComponents()
+        view.backgroundColor = .white
+        tabBar.tintColor = .black
+        tabBar.unselectedItemTintColor = .black
+    }
 
+    private func setupTabBarComponents(){
+        var viewControllers: [UIViewController] = []
+
+        TabBarComponent.allCases.forEach { component in
+            let viewController = component.viewController
+            viewController.tabBarItem.image = component.normalIcon
+            viewController.tabBarItem.selectedImage = component.selectedIcon
+            viewController.tabBarItem.title = component.title
+            viewControllers.append(viewController)
+        }
+
+        self.viewControllers = viewControllers
     }
 }

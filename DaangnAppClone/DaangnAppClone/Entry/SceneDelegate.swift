8ae97814baebd230e4015d707e483e53aeff6a8d
Ellen J <jcrescent61@gmail.com>
2024-08-23 18:18:23 +0900

    refactor: 앱 테마 밝은 테마로 고정

--- DaangnAppClone/DaangnAppClone/Entry/SceneDelegate.swift
+++ DaangnAppClone/DaangnAppClone/Entry/SceneDelegate.swift
@@ -23,6 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             rootViewController: TabBarViewController()
         )
         window?.makeKeyAndVisible()
+        window?.overrideUserInterfaceStyle = .light
     }
 }
 
